3d55f2520d0eb058b52a0cac9e28e8be28fafb77
redto0 <aboccacc@umich.edu>
2026-02-14 14:55:08 -0500

    gps waypoint struct

---
+++ include/yet_another_gps_publisher/gps_waypoint.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <geometry_msgs/msg/pose.hpp>
+
+class gps_waypoint
+{
+public:
+    gps_waypoint() = default;
+    gps_waypoint(double lon, double lat, const std::string& method, double radius = 0.0);
+
+    double longitude() const { return longitude_; }
+    double latitude() const { return latitude_; }
+    const std::string& method() const { return method_; }
+    double radius() const { return radius_; }
+    bool enabled() const { return enabled_; }
+    void setEnabled(bool e) { enabled_ = e; }
+
+    // Odom pose after transformation (set when waypoint is loaded)
+    void setOdomPose(const geometry_msgs::msg::Pose& pose) { odom_pose_ = pose; }
+    const geometry_msgs::msg::Pose& odomPose() const { return odom_pose_; }
+
+private:
+    double longitude_ = 0.0;
+    double latitude_ = 0.0;
+    std::string method_ = "linear";
+    double radius_ = 0.0;
+    bool enabled_ = true;
+    geometry_msgs::msg::Pose odom_pose_;
+};
